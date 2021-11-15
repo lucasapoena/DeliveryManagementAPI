1ce18794ad09b7bbfb5aa079529800ecced0925e
Lucas Apoena <lucasapoena@gmail.com>
2021-11-15 18:07:43 -0300

    Domain :: Implementação das entidades bases

---
+++ src/Domain/Contracts/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Domain.Contracts
+{
+    public abstract class Entity : IEntity {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
