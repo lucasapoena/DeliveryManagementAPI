8f8b51fb13f4cf825a6d350d4464e1e53b105255
Lucas Apoena <lucasapoena@gmail.com>
2021-11-15 22:25:03 -0300

    Infraestruture :: Implementação do Context e mapeamento do EF

---
+++ src/Infrastructure/Persistence/EF/ApiDBContext.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.EF
+{
+    public class ApiDBContext : DbContext
+    {
+        public ApiDBContext(DbContextOptions<ApiDBContext> options)
+           : base(options)
+        { }
+
+        public DbSet<ImportDelivery> ImportDeliveries { get; set; }
+        public DbSet<ImportDeliveryItem> ImportDeliveryItens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            /* 
+             * ----- Classes Ignoradas -----
+             * Remove a classe de Notificação e os ValueObjects para não possuirem tabelas na base de dados.
+             * -----------------------------
+             */
+            //modelBuilder.Ignore<Password>();
+
+
+            // ------ Mapeia as entidades
+            #region Adiciona entidades mapeadas - Automaticamente via Assembly
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApiDBContext).Assembly);
+            #endregion
+            // ------ Fim - Mapeia as entidades
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ src/Infrastructure/Persistence/EF/Map/ImportDeliveryMap.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.EF.Map
+{
+    public class ImportDeliveryMap : IEntityTypeConfiguration<ImportDelivery>
+    {
+        public void Configure(EntityTypeBuilder<ImportDelivery> builder)
+        {
+            #region "Nome da Tabela"
+            builder.ToTable("ImportDeliveries");
+            #endregion
+
+            #region "Primary Key"
+            builder.HasKey(x => x.Id);
+            #endregion
+
+            #region "Mapeamento das Propriedades da Entity"
+            builder.Property(x => x.ImportDate)                
+                .IsRequired();
+
+            builder.Property(x => x.TotalItens)
+                .IsRequired();
+
+            builder.Property(x => x.MinimalDeliveryDate)
+                .IsRequired();
+
+            builder.Property(x => x.TotalDeliveryItens)
+                .IsRequired();
+            #endregion
+
+            #region "Mapeamento dos ValueObject"
+            // ---- N/A -----
+            #endregion
+
+            #region "Mapeamento dos Relacionamentos com outras entidades"
+            // ---- N/A -----
+            #endregion
+        }
+    }
+}
