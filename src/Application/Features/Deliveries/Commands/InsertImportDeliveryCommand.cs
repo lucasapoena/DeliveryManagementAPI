f18b3dd26e8499d450f6b6d16db89debacc30da2
Lucas Apoena <lucasapoena@gmail.com>
2021-11-15 22:27:04 -0300

    Application :: Implementação inicial do command e do handler de insert

---
+++ src/Application/Features/Deliveries/Commands/InsertImportDeliveryCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Shared.Wrapper;
+using System;
+
+namespace Application.Features.Deliveries.Commands
+{
+    public class InsertImportDeliveryCommand : IRequest<Result<Guid>>
+    {        
+        public IFormFile File { get; set; }
+    }
+}
---
+++ src/Application/Features/Deliveries/Handlers/InsertImportDeliveryCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.Deliveries.Commands;
+using MediatR;
+using Shared.Wrapper;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.Deliveries.Handlers
+{
+    public class InsertImportDeliveryCommandHandler : IRequestHandler<InsertImportDeliveryCommand, Result<Guid>>
+    {
+        public async Task<Result<Guid>> Handle(InsertImportDeliveryCommand command, CancellationToken cancellationToken)
+        {
+            if (command.File == null || command.File.Length == 0)
+            {
+                return await Result<Guid>.FailAsync("Arquivo em branco");
+            }
+
+            return await Result<Guid>.SuccessAsync("teste");
+        }
+    }
+}
