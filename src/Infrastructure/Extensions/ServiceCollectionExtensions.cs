b26a70002e245834dc68c8b49d3900ce38127410
Lucas Apoena <lucasapoena@gmail.com>
2021-11-16 16:46:28 -0300

    Infra :: Implementação do método de extensão com os service locator para os repositories

---
+++ src/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Application.Interfaces.Repositories;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            return services
+                .AddTransient<IImportDeliveryRepository, ImportDeliveryRepository>()
+                .AddTransient<IImportDeliveryItemRepository, ImportDeliveryItemRepository>();
+        }
+    }
+}
