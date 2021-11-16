86497964ed62b2c9de2049f5993676272a515c81
Lucas Apoena <lucasapoena@gmail.com>
2021-11-15 22:24:07 -0300

    API :: Implementação inicial do controller e métodos de extensão

---
+++ src/API/Controllers/ImportDeliveryController.cs
@@ -0,0 +1,62 @@
+﻿using Application.Features.Deliveries.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    /// <summary>
+    /// ImportDelivery Controller Class
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class ImportDeliveryController : ControllerBase
+    {
+        /// <summary>
+        /// Insert an import file
+        /// </summary>
+        /// <param name="mediator"></param>
+        /// <param name="file"></param>
+        /// <returns>Status 200 OK</returns>
+        /// <returns>Status 400 BadRequest</returns>
+        [HttpPost]
+        [Route("Insert")]
+        public async Task<IActionResult> Insert(
+            [FromServices] IMediator mediator, 
+            IFormFile file)
+        {
+            var result = await mediator.Send(new InsertImportDeliveryCommand { File = file });
+            return result.Succeeded ? Ok(result) : BadRequest(result);
+        }
+
+
+        /// <summary>
+        /// Get All Import
+        /// </summary>
+        /// <returns>Status 200 Ok</returns>
+        [HttpGet]
+        [Route("GetAllImports")]
+        public async Task<IActionResult> GetAllImports([FromServices] IMediator mediator)
+        {
+            var imports = "";
+            //var imports = await mediator.Send(new GetAllDeliveriesImport());
+            return Ok(imports);
+        }
+
+        /// <summary>
+        /// Get a Import By Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Status 200 Ok</returns>
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public async Task<IActionResult> GetById([FromServices] IMediator mediator, Guid id)
+        {
+            var import = "";
+            //var import = await mediator.Send(new GetImportByIdQuery() { Id = id });
+            return Ok(import);
+        }
+    }
+}
--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -1,16 +1,9 @@
+using API.Extensions;
+using Application.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace API
 {
@@ -23,37 +16,25 @@ namespace API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
+           // services.AddDatabaseMemory();
+            services.AddApplicationLayer();
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+
+            services.RegisterSwagger();            
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
-
+            app.UseCors();
+            app.UseExceptionHandling(env);
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints();
+            app.ConfigureSwagger();
         }
     }
 }
