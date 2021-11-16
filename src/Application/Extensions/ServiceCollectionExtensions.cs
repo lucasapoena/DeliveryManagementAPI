97477ec08ed716040159194d9d9b1b8126d1ebd6
Lucas Apoena <lucasapoena@gmail.com>
2021-11-15 22:26:28 -0300

    Application :: Método de extensão da chamada do MediatR

---
+++ src/Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+           // services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
